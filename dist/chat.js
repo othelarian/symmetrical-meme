7373db74a467bec6d7428c26060520ef9d6fb1b2
othelarian <le.maitre.killian@gmail.com>
2022-04-23 00:38:07 +0200

    fix #6, fix #7, début sur #2

--- dist/chat.js
+++ dist/chat.js
@@ -1,22 +1,67 @@
 var ws;
+var chat_zone;
+var inp;
+
 function initchat() {
-  //
-  // TODO: préparation du formulaire
-  //
-  //
-  // websocket config
+  chat_zone = document.getElementById("chat_zone");
+  inp = document.getElementById("inp");
+  inp.addEventListener("keyup",
+    (evt) => { if (evt.code == "Enter") {send_msg()}});
   ws = new WebSocket("ws://localhost:3030/ws");
-  ws.onopen = function(evt) {
-    //
-    console.log("ws opened");
-    console.log(evt);
-    //
+  ws.onmessage = handle_msg;
+  ws.onopen = function(_evt) {
+    ws.send(JSON.stringify({"msg_type":"Command","content":"populate"}));
   }
-  ws.onmessage = function(evt) {
-    //
-    //
+}
+
+function handle_msg(evt) {
+  let msg = JSON.parse(evt.data);
+  switch (msg.msg_type) {
+    case "Command":
+      if (msg.content == "close") {
+        add_entry("end", "Server disconnected");
+        ws.close();
+        let r = new XMLHttpRequest();
+        r.open("GET", "http://localhost:3030/stop", false);
+        try { r.send(); } catch { console.log("failed"); }
+      } else if (msg.content[0] == "#") {
+        document.getElementById("user_id").innerText = msg.content[1];
+        add_entry("cmd", `Welcome! You're user #${msg.content[1]}`);
+      } else if (msg.content.substring(0, 3) == "add") {
+        let user_id = msg.content.split(":")[1];
+        add_entry("cmd", `User #${user_id} joined the chat`);
+      } else if (msg.content.substring(0, 4) == "quit") {
+        let user_id = msg.content.split(":")[1];
+        add_entry("cmd", `User #${user_id} leaved the chat`);
+      } else if (msg.content.substring(0, 3) == "pop") {
+        let p = JSON.parse(msg.content.split(":")[1]);
+        if (p.length) {
+          p = p.map((u) => "user "+u).join(", ");
+          add_entry("cmd", `The other user are: ${p}`);
+        } else {
+          add_entry("cmd", "You're alone here :(");
+        }
+      }
+      break;
+    case "Text":
+      add_entry("", msg.content);
+      break;
   }
-  ws.onclose = function(_evt) {
-    console.log("ws closed");
+}
+
+function add_entry(e_class, text) {
+  let d = document.createElement("div");
+  d.setAttribute("class", e_class);
+  d.innerText = text;
+  chat_zone.append(d);
+}
+
+function send_msg() {
+  let v = inp.value;
+  if (v != "") {
+    add_entry("msg", `You: ${v}`);
+    let msg= {"msg_type": "Text", "content": v};
+    ws.send(JSON.stringify(msg));
+    inp.value = "";
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use gluon::{ThreadExt, new_vm};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fs::File;
 use std::sync::{Arc, atomic::{AtomicUsize, Ordering}};
 use tokio::{
   sync::{RwLock, mpsc::{unbounded_channel, UnboundedSender}},
@@ -6,7 +9,7 @@ use tokio::{
 };
 use tokio_stream::wrappers::UnboundedReceiverStream;
 use warp::{Filter, ws::{Message, WebSocket, Ws}};
-use futures_util::{SinkExt, StreamExt, TryFutureExt};
+use futures_util::{SinkExt, StreamExt};
 
 #[cfg(windows)]
 mod win_tray;
@@ -21,6 +24,7 @@ type WsPool = Arc<RwLock<HashMap<usize, UnboundedSender<Message>>>>;
 async fn main() {
   // arc config
   let wspool = WsPool::default();
+  let wspool_q = wspool.clone();
   // quit channel
   let (tx, rx) = unbounded_channel();
   let mut  rx = UnboundedReceiverStream::new(rx);
@@ -33,26 +37,28 @@ async fn main() {
     tx.send(()).unwrap();
     "quit ok"
   });
-  let wspool = warp::any().map(move || wspool.clone());
+  let wspool_w = warp::any().map(move || wspool.clone());
   let ws = warp::path("ws")
     .and(warp::ws())
-    .and(wspool)
-    .map(|ws: Ws, wspool|
-      ws.on_upgrade(move |socket| ws_handler(socket, wspool))
+    .and(wspool_w)
+    .map(|ws: Ws, wspool_w|
+      ws.on_upgrade(move |socket| ws_handler(socket, wspool_w))
     );
+  let stop = warp::path("stop").map(move || {"stop"});
   let dir = warp::fs::dir("./dist");
   let others = warp::any().and(warp::fs::file("./dist/chat.html"));
-  let routes = quit.or(ws).or(dir).or(others);
+  let routes = quit.or(stop).or(ws).or(dir).or(others);
   // launching server
   let (_, svr) = warp::serve(routes)
     .bind_with_graceful_shutdown(
       ([127, 0, 0, 1], 3030),
       async move {
         rx.next().await;
-        //
-        // TODO: ext-ce que c'est ici qu'on traite les ws ?
-        println!("shutdown activated");
-        //
+        let msg = WsMessage::new(WsMessageType::Command, String::from("close"));
+        let msg = serde_json::to_string(&msg).unwrap();
+        wspool_q.read().await.values().for_each(|u_tx|
+          u_tx.send(Message::text(&msg)).unwrap()
+        );
       }
     );
   //#[cfg(windows)]
@@ -60,25 +66,101 @@ async fn main() {
   let _ = svr.await;
 }
 
+#[derive(Deserialize, Serialize)]
+enum WsMessageType {
+  Command,
+  Text
+}
+
+#[derive(Deserialize, Serialize)]
+struct WsMessage {
+  msg_type: WsMessageType,
+  content: String
+}
+
+impl WsMessage {
+  fn new(msg_type: WsMessageType, content: String) -> Self {
+    Self { msg_type, content }
+  }
+}
+
 async fn ws_handler(socket: WebSocket, wspool: WsPool) {
+  // config
   let ws_id = NEXT_WSP_ID.fetch_add(1, Ordering::Relaxed);
   let (mut ws_tx, mut ws_rx) = socket.split();
   let (u_tx, u_rx) = unbounded_channel();
   let mut u_rx = UnboundedReceiverStream::new(u_rx);
+  let gl_vm = new_vm();
+  // saying hello to everyone
+  let msg = WsMessage::new(
+    WsMessageType::Command, format!("add:{}", ws_id));
+  let msg = Message::text(&serde_json::to_string(&msg).unwrap());
+  wspool.read().await.values()
+    .for_each(|u_tx| u_tx.send(msg.clone()).unwrap_or_else(|_| ()));
+  // saving id in the hashmap
   wspool.write().await.insert(ws_id, u_tx.clone());
-  //
-  // TODO: envoi d'un message à tout les ws
-  //
-  //
-  //
+  // listening from other ws
   task::spawn(async move { while let Some(msg) = u_rx.next().await {
-    ws_tx.send(msg).unwrap_or_else(|e| eprintln!("ws error: {}", e)).await;
+    match ws_tx.send(msg).await {
+      Ok(_) => (),
+      Err(_e) => { /*eprintln!("error (#{}): {}", ws_id, e);*/ break; }
+    }
   }});
-  while let Some(msg) = ws_rx.next().await {
-    //
-    println!("ws used");
-    //
-    //
+  // sending id to the user
+  let msg = WsMessage::new(
+    WsMessageType::Command, format!("#{}", ws_id));
+  let msg = Message::text(&serde_json::to_string(&msg).unwrap());
+  u_tx.send(msg).unwrap();
+  // dialog loop
+  while let Some(res) = ws_rx.next().await {
+    let msg = match res {
+      Ok(msg) => {
+        if msg.is_close() {
+          let msg = Message::text(&serde_json::to_string(
+            &WsMessage::new(WsMessageType::Command, format!("quit:{}", ws_id))
+          ).unwrap());
+          wspool.read().await.values()
+            .for_each(|u_tx| u_tx.send(msg.clone()).unwrap_or_else(|_| ()));
+          break;
+        } else { msg.to_str().unwrap().to_string() }
+      }
+      Err(e) => { eprintln!("ws err: {}", e); break; }
+    };
+    let res: WsMessage = serde_json::from_str(&msg).unwrap();
+    match res.msg_type {
+      WsMessageType::Command => {
+        if res.content == "populate" {
+          let reader = wspool.read().await;
+          let mut list = reader.keys().filter(|k| *k != &ws_id)
+            .collect::<Vec<_>>();
+          list.sort();
+          let msg = WsMessage::new(WsMessageType::Command,
+            format!("pop:{}", serde_json::to_string(&list).unwrap()));
+          let msg = Message::text(&serde_json::to_string(&msg).unwrap());
+          u_tx.send(msg).unwrap_or_else(|_| ());
+        }
+      }
+      WsMessageType::Text => {
+        //
+        // TODO
+        //
+        //println!("Text: {}", res.content);
+        //
+        gl_vm.load_file("scripts/parse.glu").unwrap();
+        //
+        //
+        let tr = res.content;
+        //
+        let msg = WsMessage::new(WsMessageType::Text,
+          format!("User #{}: {}", ws_id, tr));
+        let msg = Message::text(serde_json::to_string(&msg).unwrap());
+        let reader = wspool.read().await;
+        for (_, u_tx) in reader.iter().filter(|(id, _)| *id != &ws_id) {
+          u_tx.send(msg.clone()).unwrap_or_else(|_| ());
+        }
+      }
+    }
   }
+  // bye
   wspool.write().await.remove(&ws_id);
 }
